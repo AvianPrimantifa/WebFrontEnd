02b9f744e674e0cf8a73fea10d32ff121f7b6879
Avian Putra Primantifa <Primantifa@android-5f27793e94f4e53b.icehousecorp.local>
2015-07-09 15:28:54 +0700

    Initial commit.

---
+++ script.js
@@ -0,0 +1,8 @@
+//alert("My first Javascript");
+
+document.getElementById('header').innerHTML = "This Is Just Normal Header";
+
+function changeFooterBG()
+{
+    document.getElementById('footer').style.backgroundColor="red";
+}
