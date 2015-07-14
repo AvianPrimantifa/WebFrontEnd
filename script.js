a394ef3469813fb500dab03d27f3ec90c7fca436
Avian Putra Primantifa <Primantifa@android-5f27793e94f4e53b.icehousecorp.local>
2015-07-14 12:37:38 +0700

    preprocessor

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 //alert("My first Javascript");
 
-document.getElementById('header').innerHTML = "This Is Just Normal Header";
+//document.getElementById('header').innerHTML = "This Is Just Normal Header";
 
 function changeFooterBG()
 {
     document.getElementById('footer').style.backgroundColor="red";
+}
+
+function countClick()
+{
+    document.getElementById('counter').value = "it has been clicked!";
+
+
+
 }
