04134446df4493a83f8ab9d2adeab6c9aa2a9df3
Avian Putra Primantifa <Primantifa@android-49e4613c48eeeb6.icehousecorp.local>
2015-08-19 15:13:21 +0700

    modal at the left side and hover animation on thumbnails

--- script.js
+++ script.js
@@ -1,16 +1,40 @@
+$(document).ready(function(){
+
+
+    $(".pict").each(function(idx,val){
+        var $desc = $(this).find(".desc");
+        var $feat = $(this).find(".feat");
+        var $featbutton = $(this).find(".featbutton");
+
+        $(this).hover(function(){
+            $desc.fadeOut();
+            $feat.fadeIn();
+            $featbutton.fadeIn();
+
+        },function(){
+            $desc.fadeIn();
+            $feat.fadeOut();
+            $featbutton.fadeOut();
+
+
+        });
+    });
+
+
+});
 //alert("My first Javascript");
 
 //document.getElementById('header').innerHTML = "This Is Just Normal Header";
 
-function changeFooterBG()
-{
-    document.getElementById('footer').style.backgroundColor="red";
-}
-
-function countClick()
-{
-    document.getElementById('counter').value = "it has been clicked!";
-
-
-
-}
+// function changeFooterBG()
+// {
+//     document.getElementById('footer').style.backgroundColor="red";
+// }
+//
+// function countClick()
+// {
+//     document.getElementById('counter').value = "it has been clicked!";
+//
+//
+//
+// }
